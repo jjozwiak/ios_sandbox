05752a8fa9ab6bd2b4fd001ed82acb3fb6a7b875
jjozwiak <jason.jozwiak@gmail.com>
2012-09-19 21:50:30 -0500

    updating repo

---
+++ HypnoTime/HypnoTime/HypnoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnoAppDelegate.h
+//  HypnoTime
+//
+//  Created by Jason Jozwiak on 9/19/12.
+//  Copyright (c) 2012 Jason Jozwiak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
